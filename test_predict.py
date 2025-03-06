1203adc5ed15918c082269c090f88bd0294572d8
Hemalatha M <hemalatham236@gmail.com>
2025-03-06 17:40:54 +0530

    save

--- test_predict.py
+++ test_predict.py
@@ -9,7 +9,7 @@ def client():
 def test_pinger(client):
     resp = client.get('/ping')
     assert resp.status_code == 200
-    assert resp.json == {"MESSAGE" : "Hi, I am Pinging p78888...!!!!!"}
+    assert resp.json == {"MESSAGE" : "Hi, I am Pinging ...!!!!!"}
 
 
 def test_predict(client):
